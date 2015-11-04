a327b488366a540230c38dea35c1dbce389b57b9
teruki-i <ito.teruki@gmail.com>
2015-11-03 23:55:06 -0800

    Create plot1.R
    This code creates Plot 1 and the PNG image file of the plot.

---
+++ plot1.R
@@ -0,0 +1,30 @@
+datalink<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if(!file.exists("epc")){
+    download.file(datalink,destfile="epc.zip")
+    unzip("epc.zip",exdir = "epc")
+}
+
+hpc<-read.table("epc/household_power_consumption.txt",skip=66637, nrow=2880, sep=";",col.names = colnames(
+    read.table("epc/household_power_consumption.txt", nrow = 1, header = TRUE, sep=";")))
+##reads data out of txt file only for rows dated 1/2/2007 and 2/2/2007
+
+hpc$Date<-as.Date(hpc$Date,"%d/%m/%Y")
+##"Date" column was originally a Factor variable, so converted to Date
+
+x<-paste(hpc$Date,hpc$Time)
+hpc$Time<-strptime(x,"%Y-%m-%d %H:%M:%S")
+
+hist(hpc$Global_active_power,
+     xlab="Global Active Power (kilowatts)",
+     main="Global Active Power",
+     col = "red")
+
+
+
+png(file="plot1.png")
+with(hpc, hist(hpc$Global_active_power,
+               xlab="Global Active Power (kilowatts)",
+               main="Global Active Power",
+               col = "red"))
+dev.off()
